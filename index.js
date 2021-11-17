968a1652ad913bc91a59ec501eeafc682406eec8
CharlieEllenbecker <57051469+CharlieEllenbecker@users.noreply.github.com>
2021-11-17 09:33:57 -0600

    added folders, decks, and cards routes to index.js to be defined later

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+const folders = require('/routes/folders');
+const decks = require('./routes/decks');
+const cards = require('./routes/cards');
+
 const express = require('express');
 const app = express();
 
+
 app.use(express.json());
+app.use('/api/folders', folders);
+app.use('/api/decks', decks);
+app.use('/api/cards', cards);
 
 
 const port = process.env.PORT || 3000;
