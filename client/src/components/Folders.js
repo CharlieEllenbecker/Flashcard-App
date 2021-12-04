0a7862f902a33551fb5936b5b6a18abab11a2ac5
CharlieEllenbecker <57051469+CharlieEllenbecker@users.noreply.github.com>
2021-12-03 19:26:16 -0600

    started working on the folders component to display the folders

---
+++ client/src/components/Folders.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
+function Folders() {
+    const [folders, setFolders] = useState([]);
+
+    useEffect(() => {
+        fetch('/api/folders')
+            .then(res => res.json())
+            .then(folders => setFolders(folders));
+            // .catch()
+    });
+
+    return(
+        <div>
+            <h1>Folders</h1>
+            <ul>
+                {folders.map(f => <li>{f.name}</li>)}
+            </ul>
+        </div>
+    );
+}
+
+export default Folders;
